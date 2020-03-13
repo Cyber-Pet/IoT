e2d51da6f53a788a55792b8eaa0e0adc59d3ee19
Gabriel Meyer <ghmeyer0@gmail.com>
2020-03-13 13:53:24 -0300

    Adicionado Codigos e links uteis

--- src/code.js
+++ src/code.js
@@ -1,4 +1,25 @@
-// ESP8266
+// Conectar no Wifi
+const wifi = require("Wifi");
+wifi.connect("my-ssid", {password:"my-pwd"}, function(err){
+  console.log("connected? err=", err, "info=", wifi.getIP());
+});
+wifi.stopAP();
+
+// Criar ponto de acesso
+// var wifi = require("Wifi");
+// wifi.disconnect();
+// wifi.startAP("my-ssid", {password:"my-password",authMode:"wpa_wpa2"});
+
+
+// Fazer Requisisão para API
+// var http = require("http");
+// http.get("http://www.espruino.com", function(res) {
+//   res.on('data', function(data) {
+//     console.log(data);
+//   });
+// });
+
+function onInit() {
  const pin = 2;
  let on = 0;
  digitalWrite(pin, on);
@@ -6,3 +27,5 @@
    on = on === 0 ? 1 : 0;
    digitalWrite(pin, on);
  }, 100);
+}
+save();
