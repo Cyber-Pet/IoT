51975b4501d430e9e74d72d91b2a88baa2fdc4b9
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2020-03-17 20:09:53 -0300

    Index atualizado para mover Servo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const wifi = require("Wifi");
 const http = require("http");
+const servoPin = 16;
+const servo = require("servo").connect(servoPin);
 const wifiApToConnect = "CYBER-PET";
 const wifiOption = {password: "pet-cyber", authMode:"wpa_wpa2"};
 const wifiAp = "ESP8266";
@@ -16,22 +18,29 @@ function onInit() {
   // Conectar na rede e habilitar hotspot ao iniciar
   wifi.connect(wifiApToConnect,{password: "pet-cyber"});
   wifi.startAP(wifiAp, wifiOption);
-  console.log("Conectado na rede " + wifiApToConnect + "!");
-  wifi.getIP((err, data) => console.log("IP address: " + data.ip)); 
+  wifi.getIP((err, data) => console.log("IP address: " + data.ip));
 }
 
-// Led liga quando o ESP conecta na rede wi-fi
+// O led liga quando o ESP conecta na rede wi-fi
 wifi.on('connected',() => {
   http.get("https://ghmeyer0.free.beeceptor.com", () => {
   console.log("Conectado na rede " + wifiApToConnect + "!");
   wifi.getIP((err, data) => console.log("IP address: " + data.ip)); 
+  // Liga led
   digitalWrite(2, 0);
+  // Move servo
+  servo.move(0);
   });
 });
 
-// Led desliga quando o ESP conecta na rede wi-fi
+// Led desliga quando o ESP desconecta da rede wi-fi
 wifi.on('disconnected',() => {
+  // Desliga led
   digitalWrite(2, 1);
+  // Move servo
+  servo.move(1);
 });
+
+//SERVO rules
 
 save();
