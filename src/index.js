b8302176f5794aed8129963c2c2e00776c1fc3ba
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2020-03-16 21:54:39 -0300

    Embelezamento index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-  const wifi = require("Wifi");
-  const http = require("http");
-  const wifiApToConnect = "CYBER-PET";
-  const wifiOption = {password: "pet-cyber", authMode:"wpa_wpa2"};
-  const wifiAp = "ESP8266";
+const wifi = require("Wifi");
+const http = require("http");
+const wifiApToConnect = "CYBER-PET";
+const wifiOption = {password: "pet-cyber", authMode:"wpa_wpa2"};
+const wifiAp = "ESP8266";
 
 // Fazer Requisisão para API
 // var http = require("http"a);
@@ -13,25 +13,25 @@
 // });
 
 function onInit() {
-  // Conectar rede e habilitar hotspot
+  // Conectar na rede e habilitar hotspot ao iniciar
   wifi.connect(wifiApToConnect,{password: "pet-cyber"});
   wifi.startAP(wifiAp, wifiOption);
   console.log("Conectado na rede " + wifiApToConnect + "!");
   wifi.getIP((err, data) => console.log("IP address: " + data.ip)); 
 }
 
-  // Led liga quando o ESP conecta na rede wi-fi
-  wifi.on('connected',() => {
-    http.get("https://ghmeyer0.free.beeceptor.com", () => {
-      console.log("Conectado na rede " + wifiApToConnect + "!");
-      wifi.getIP((err, data) => console.log("IP address: " + data.ip)); 
-      digitalWrite(2, 0);
-    });
+// Led liga quando o ESP conecta na rede wi-fi
+wifi.on('connected',() => {
+  http.get("https://ghmeyer0.free.beeceptor.com", () => {
+  console.log("Conectado na rede " + wifiApToConnect + "!");
+  wifi.getIP((err, data) => console.log("IP address: " + data.ip)); 
+  digitalWrite(2, 0);
   });
+});
 
-  // Led desliga quando o ESP conecta na rede wi-fi
-  wifi.on('disconnected',() => {
-    digitalWrite(2, 1);
-  });
+// Led desliga quando o ESP conecta na rede wi-fi
+wifi.on('disconnected',() => {
+  digitalWrite(2, 1);
+});
 
 save();
