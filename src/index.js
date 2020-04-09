5c1e25877d7c364ac3029572ecc662695969de9d
Reinert <gustavo.reinert@thomsonreuters.com>
2020-04-08 23:31:36 -0300

    Atualizando do servo no index

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ const wifiOption = {password: "pet-cyber", authMode:"wpa_wpa2"};
 const wifiAp = "ESP8266";
 
 // Fazer requisisão para API
-// var http = require("http"a);
-// http.get("http://www.espruino.com", function(res) {
-//   res.on('data', function(data) {
-//     console.log(data);
-//   });
-// });
+// const http = require("http");
+// http.get("http://www.espruino.com", () => {
+// your code here
+//  });
 
 // Faz um scan das redes disponíveis e imprime no terminal
 // wifi.scan(function(scan) {
@@ -20,31 +18,33 @@ const wifiAp = "ESP8266";
 // });
 
 function onInit() {
-  // Conectar na rede, habilita hotspot e imprime o endereço de IP ao iniciar
-  wifi.connect(wifiApToConnect,{password: "pet-cyber"});
-  wifi.startAP(wifiAp, wifiOption);
-  console.log("IP address: " + wifi.getIP().ip);
+    // Conectar na rede, habilita hotspot e imprime o endereço de IP ao iniciar
+    wifi.connect(wifiApToConnect,{password: "pet-cyber"});
+    wifi.startAP(wifiAp, wifiOption);
+    console.log("IP address: " + wifi.getIP().ip);
 }
 
 // Led liga quando o ESP conecta na rede wi-fi
 wifi.on('connected',() => {
-  http.get("https://ghmeyer0.free.beeceptor.com", () => {
-    // Imprime no terminal em qual rede está conectado e o endereço de IP
-    console.log("Conectado na rede " + wifiApToConnect + "!");
-    console.log("IP address: " + wifi.getIP().ip);
-    // Liga led
-    digitalWrite(2, 0);
-    // Move servo
-    servo.move(0);
-  });
+    http.get("https://ghmeyer0.free.beeceptor.com", () => {
+        // Imprime no terminal em qual rede está conectado e o endereço de IP
+        console.log("Conectado na rede " + wifiApToConnect + "!");
+        console.log("IP address: " + wifi.getIP().ip);
+        // Liga led
+        digitalWrite(2, 0);
+        // Move servo
+        servo.move(1);
+    });
 });
 
 // Led desliga quando o ESP desconecta da rede wi-fi
 wifi.on('disconnected',() => {
-  // Desliga led
-  digitalWrite(2, 1);
-  // Move servo
-  servo.move(1);
+    http.get("https://ghmeyer0.free.beeceptor.com", () => {
+        // Desliga led
+        digitalWrite(2, 1);
+        // Move servo
+    servo.move(0);
+    });
 });
 
 //SERVO rules
