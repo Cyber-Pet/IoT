716cbcfc8bd282221240850fa3447c113efe708a
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2020-06-19 20:17:01 -0300

    MQTT implementation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const wifi = require("Wifi");
 const http = require("http");
 const servoPin = 16;
 const servo = require("servo").connect(servoPin);
+
+// Wifi config
 const wifiApToConnect = "CYBER-PET";
-const wifiOption = {password: "pet-cyber", authMode:"wpa_wpa2"};
+const wifiOption = { password: "pet-cyber", authMode: "wpa_wpa2" };
 const wifiAp = "ESP8266";
+
+// MQTT config
+var server = "test.mosquitto.org"; // the ip of your MQTT broker
+var options = { // ALL OPTIONAL - the defaults are below
+    client_id: "5ccf7f4c-f248",   // the client ID sent to MQTT - it's a good idea to define your own static one based on `getSerial()`
+    keep_alive: 60,         // keep alive time in seconds
+    port: 1883,             // port number
+};
+var mqtt = require("MQTT").create(server, options);
 
 // Fazer requisisão para API
 // const http = require("http");
@@ -17,38 +28,47 @@ const wifiAp = "ESP8266";
 //   console.log(scan)
 // });
 
+function moveServo() {
+    servo.move(1, 1050);
+}
+
 function onInit() {
     // Conectar na rede, habilita hotspot e imprime o endereço de IP ao iniciar
-    wifi.connect(wifiApToConnect,{password: "pet-cyber"});
+    wifi.connect(wifiApToConnect, { password: "pet-cyber" });
     wifi.startAP(wifiAp, wifiOption);
-    console.log("IP address: " + wifi.getIP().ip);
+    console.log(getSerial());
+    console.log("On Init: IP address: " + wifi.getIP().ip);
+    mqtt.connect();
 }
 
 // Led liga quando o ESP conecta na rede wi-fi
-wifi.on('connected',() => {
-    http.get("https://ghmeyer0.free.beeceptor.com", () => {
-        // Imprime no terminal em qual rede está conectado e o endereço de IP
-        console.log("Conectado na rede " + wifiApToConnect + "!");
-        console.log("IP address: " + wifi.getIP().ip);
-        // Liga led
-        digitalWrite(2, 0);
-        // Move servo
-        servo.move(1);
-    });
+wifi.on('connected', () => {
+    // Imprime no terminal em qual rede está conectado e o endereço de IP
+    console.log("Connected: Conectado na rede " + wifiApToConnect + "!");
+    console.log("Connected: IP address: " + wifi.getIP().ip);
+    // Liga led
+    digitalWrite(2, 0);
+    // Move servo
+    servo.move(1);
 });
 
 // Led desliga quando o ESP desconecta da rede wi-fi
-wifi.on('disconnected',() => {
-    http.get("https://ghmeyer0.free.beeceptor.com", () => {
-        // Desliga led
-        digitalWrite(2, 1);
-        // Move servo
+wifi.on('disconnected', () => {
+    // Desliga led
+    digitalWrite(2, 1);
+    // Move servo
     servo.move(0);
-    });
 });
 
-//SERVO rules
+//MQTT
+mqtt.on('connected', function () {
+    mqtt.subscribe("run-bowl");
+    console.log("MQTT ALIVE!");
+});
 
-servo.move(0,500); // move to position 0 over 5 seconds
+mqtt.on('publish', function (pub) {
+    console.log("Return MQTT: " + pub.message);
+    moveServo();
+});
 
 save();
