bb0f8210ddcabde75b79de593690cffba83eb9ad
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2020-06-19 20:21:08 -0300

    Beautify code and tiny settings

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ const http = require("http");
 const servoPin = 16;
 const servo = require("servo").connect(servoPin);
 
-// Wifi config
+// Configuração do Wifi
 const wifiApToConnect = "CYBER-PET";
 const wifiOption = { password: "pet-cyber", authMode: "wpa_wpa2" };
 const wifiAp = "ESP8266";
 
-// MQTT config
-var server = "test.mosquitto.org"; // the ip of your MQTT broker
-var options = { // ALL OPTIONAL - the defaults are below
-    client_id: "5ccf7f4c-f248",   // the client ID sent to MQTT - it's a good idea to define your own static one based on `getSerial()`
-    keep_alive: 60,         // keep alive time in seconds
-    port: 1883,             // port number
+// Configuração do MQTT
+var server = "test.mosquitto.org"; // IP do servidor MQTT
+var options = { 
+    client_id: "5ccf7f4c-f248",   // Client ID enviado para o servidor do MQTT (no caso, o retorno do comando getSerial();)
+    keep_alive: 60,         // Tempo do Keep Alive
+    port: 1883,             // Número de porta
 };
+var topic = "run-bowl";
 var mqtt = require("MQTT").create(server, options);
 
 // Fazer requisisão para API
@@ -38,6 +39,7 @@ function onInit() {
     wifi.startAP(wifiAp, wifiOption);
     console.log(getSerial());
     console.log("On Init: IP address: " + wifi.getIP().ip);
+    // Conectar no MQTT
     mqtt.connect();
 }
 
@@ -48,21 +50,17 @@ wifi.on('connected', () => {
     console.log("Connected: IP address: " + wifi.getIP().ip);
     // Liga led
     digitalWrite(2, 0);
-    // Move servo
-    servo.move(1);
 });
 
 // Led desliga quando o ESP desconecta da rede wi-fi
 wifi.on('disconnected', () => {
     // Desliga led
     digitalWrite(2, 1);
-    // Move servo
-    servo.move(0);
 });
 
 //MQTT
 mqtt.on('connected', function () {
-    mqtt.subscribe("run-bowl");
+    mqtt.subscribe(topic);
     console.log("MQTT ALIVE!");
 });
 
