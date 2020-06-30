652598dd972221468e5ef0eaef8a926b91945cde
Gabriel Meyer <gabriel.meyer@tunesoft.com.br>
2020-06-30 01:48:08 -0300

    Finalizado projeto IoT

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import C from "./esp-wifi-manager"; //import the library
-
-function main() {
-  C();
-}
--- src/index.js
+++ src/index.js
@@ -2,28 +2,42 @@
 const wifi = require("Wifi");
 const wifiPassword = "pet-cyber";
 const wifiApToConnect = "CYBER-PET";
-const wifiOption = { password: wifiPassword, authMode: "wpa_wpa2" };
-const wifiAp = "ESP8266";
 
 // Configuração do MQTT
-var server = "test.mosquitto.org"; // IP do servidor MQTT
-var options = {
-    client_id: "5ccf7f4c-f248",
+const server = "test.mosquitto.org"; // IP do servidor MQTT
+const options = {
+    client_id: wifi.getIP().mac,
     keep_alive: 60,         // Tempo do Keep Alive
     port: 1883,             // Número de porta
 };
-var topic = "run-bowl";
-var mqtt = require("MQTT").create(server, options);
-var topicExecute = "cyber-pet/execute";
-var topicAlert = "cyber-pet/alert";
+const mqtt = require("MQTT").create(server, options);
+const topicExecute = `cyber-pet/execute/${wifi.getIP().mac}`;
+const topicAlert = "cyber-pet/alert";
 
 // Funçoes do Servo
-const servoPin = 16;
-var servo = require("servo").connect(servoPin);
 function moveServo() {
+    var servo = require("servo").connect(16);
     servo.move(1, 1050);
     mqtt.publish(topicAlert, "Pet Alimentado!");
 }
+
+//MQTT
+mqtt.on('connected', function () {
+    mqtt.subscribe(topicExecute);
+    mqtt.subscribe(topicAlert);
+});
+
+mqtt.on('publish', function (pub) {
+    if (pub.topic == topicExecute) {
+        digitalWrite(2, 1);
+        setTimeout(function(){
+          digitalWrite(2, 0);
+        },2000);
+        console.log("Return mqtt executed!");
+        moveServo();
+    }
+});
+
 
 // WIFI
 // Led liga quando o ESP conecta na rede wi-fi
@@ -36,29 +50,12 @@ wifi.on('disconnected', () => {
     digitalWrite(2, 1);
 });
 
-//MQTT
-mqtt.on('connected', function () {
-    mqtt.subscribe(topicExecute);
-    mqtt.subscribe(topicAlert);
-});
 
-mqtt.on('publish', function (pub) {
-    if (pub.topic == topicExecute) {
-        moveServo();
-        digitalWrite(2, 1);
-        console.log("Return mqtt executed!");
-    }
-});
 
 function onInit() {
     // Conectar na rede, habilita hotspot e imprime o endereço de IP ao iniciar
     wifi.connect(wifiApToConnect, { password: wifiPassword });
-    wifi.startAP(wifiAp, wifiOption);
-    console.log("IP address: " + wifi.getIP().ip);
-    // Conectar no MQTT
     mqtt.connect();
-    digitalWrite(2, 0);
-    console.log("MQTT alive!");
 }
 
 save();
